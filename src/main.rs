968b9290cd31409d372b386d7bbc74edb0bbecd1
danielvschoor <danielvanschoor1997@gmail.com>
2020-06-29 17:25:49 +0200

    .gitlab-ci.yml Remove OpenSSL dependency

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+pub mod config;
+pub mod controller;
+pub mod game;
+pub mod maps;
+mod paths;
+mod portconfig;
+pub mod proxy;
+mod result;
+pub mod sc2;
+mod sc2process;
+pub mod server;
+use pretty_env_logger;
+
+fn main() -> Result<(), String> {
+    pretty_env_logger::init();
+    let s = server::RustServer::new("127.0.0.1:8642");
+    s.run().join();
+    Ok(())
+
+
+}
